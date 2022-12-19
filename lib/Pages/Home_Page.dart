8cff707f8c01695011dfde78b75288d3b51eda96
atahtee <atatisam14@gmail.com>
2022-12-19 22:10:54 +0300

    second commit

--- lib/Pages/Home_Page.dart
+++ lib/Pages/Home_Page.dart
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
   ];
   final description = [
     "Easily find affordable hostels with "
-    "Hostel finder is a complete mobile application that helps you to find your next hostel easily",
+    "Hostel finder is a complete mobile application that enables you to find your next hostel easily",
     "Hostel finder is a complete mobile application that helps you to find your next hostel easily",
     "Hostel finder is a complete mobile application that helps you to find your next hostel easily"
   ];
